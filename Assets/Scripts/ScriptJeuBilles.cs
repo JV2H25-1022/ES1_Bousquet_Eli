f6bb2a5f0c02874980022863bcda9e6af9b6ab59
Jaajness <jaajnessmain@gmail.com>
2025-02-10 13:11:43 -0500

    Menu/UI

---
+++ Assets/Scripts/ScriptJeuBilles.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptJeuBilles : MonoBehaviour{
+
+    public GameObject debut;
+    public GameObject bouton;
+
+    private Animator ctrl;
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        ctrl = bouton.GetComponent<Animator>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void DebutJeu(){
+
+        debut.SetActive (false);
+        ctrl.SetBool("Milieu", true);
+        ctrl.SetBool("Debut", false);
+        ctrl.SetBool("Fin", false);
+
+    }
+}
